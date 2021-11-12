7d494e27c548b46b6c8e98b418b8f06553ff5ddf
Marat <farkhutdinovmr@gmail.com>
2021-11-12 15:25:39 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age;
+            short enemyCount;
+            int money;
+            uint score;
+            long timeToDeath;
+            ulong currentIndex;
+            float health;
+            bool isActive;
+            char keyCode;
+            string playerName;
+        }
+    }
+}
